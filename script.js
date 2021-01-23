54fc4c1e0fe441273e8149a94da93939cf222147
Jhonathan <jhonathanpribeiro@gmail.com>
2021-01-23 17:08:22 -0300

    alterado label do button de enviar para calcular

--- script.js
+++ script.js
@@ -1,31 +1,31 @@
 const form = document.querySelector('#formid');
 
-form.addEventListener('click',(e)=>{
+form.addEventListener('click', (e) => {
     e.preventDefault();
-    const inputPeso   = form.querySelector('#peso');
+    const inputPeso = form.querySelector('#peso');
     const inputAltura = form.querySelector('#altura');
 
     const peso = Number(inputPeso.value);
     const altura = Number(inputAltura.value);
-    
-    if(!peso){
+
+    if (!peso) {
         setResult('Peso inválido!', false);
         return;
     }
-    if(!altura){
+    if (!altura) {
         setResult('Altura inválida!', false);
         return;
     }
 
-    const imc =  getIMC(peso,altura);
+    const imc = getIMC(peso, altura);
     const indiceIMC = getIndiceIMC(imc);
-    
+
     const msg = `Seu IMC é ${imc} (${indiceIMC})`;
 
     setResult(msg, true);
-}); 
+});
 
-function getIndiceIMC(imc){
+function getIndiceIMC(imc) {
     const indice = [
         'Abaixo do peso',
         'Peso normal',
@@ -35,37 +35,35 @@ function getIndiceIMC(imc){
         'Obesidade grau 3'
     ];
 
-    if(imc >= 39.9) return indice[5];
-    if(imc >= 34.9) return indice[4]; 
-    if(imc >= 29.9) return indice[3];
-    if(imc >= 24.9) return indice[2];
-    if(imc >= 18.5) return indice[1];
-    if(imc < 18.5)  return indice[0];
+    if (imc >= 39.9) return indice[5];
+    if (imc >= 34.9) return indice[4];
+    if (imc >= 29.9) return indice[3];
+    if (imc >= 24.9) return indice[2];
+    if (imc >= 18.5) return indice[1];
+    if (imc < 18.5) return indice[0];
 }
 
-function getIMC(peso,altura){
-    const imc =  peso / altura ** 2;
+function getIMC(peso, altura) {
+    const imc = peso / altura ** 2;
     return imc.toFixed(2);
 }
 
 //criando um elemento(tag) p
-function createParagraph(className){
-      const p = document.createElement('p');
-      return p;
+function createParagraph(className) {
+    const p = document.createElement('p');
+    return p;
 }
 
 function setResult(msg, isValid) {
     const res = document.querySelector('#res');
-    res.innerHTML = '';    
+    res.innerHTML = '';
     const p = createParagraph();
 
-    if(isValid) {
+    if (isValid) {
         p.classList.add('success');
     } else {
         p.classList.add('danger');
     }
-
     p.innerHTML = msg;
     res.appendChild(p);
 }
-
